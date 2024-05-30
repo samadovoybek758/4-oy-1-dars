cbaafc13430ea1098fb416206459fe0e7d75033b
samadovoybek758 <oybek22samadov@gmail.com>
2024-05-30 18:21:13 +0500

    birinchi

---
+++ js/index.js
@@ -0,0 +1,92 @@
+// 1-masala
+// let a = + prompt("sonni kiriting");
+// let P =4*a;
+// console.log(P);
+
+
+
+// // 2-masala
+// let a = + prompt("sonni kiriting");
+// let S = a*a;
+// console.log(S);
+
+
+
+// 3-masala
+// let a = + prompt("a sonni kiriting");
+// let b = + prompt ("b sonni kiriting");
+// let S = a*b;
+// console.log(S);
+//  let P = 2*(a+b);
+//  console.log(P);
+
+
+
+// 4-masala
+// let d = + prompt("aylana diametrini kiriting");
+// console.log(Math.PI*d);
+
+
+
+// 5-masala
+// let a = + prompt(`a sonni kiriting`);
+
+// let V =a*a*a;
+// console.log(V);
+
+// let S =6*a*a;
+// console.log(S);
+
+
+
+// 6-masala
+// let a = + prompt("a sonni kiriting")
+// let b = + prompt("b sonni kiriting")
+// let c = + prompt("c sonni kiriting")
+
+// let V = a*b*c;
+// let S = 2*(a*b+b*c+a*c);
+
+// console.log(V);
+// console.log(S);
+
+
+
+// 7-masala
+// let R = + prompt(`radiusni kiriting`);
+
+// let L =2*Math.PI*R;
+// console.log(L);
+
+// let S =R*R*Math.PI;
+// console.log(S);
+
+
+
+// 8-masala
+// let a = + prompt(`a sonni kiriting`);
+// let b = + prompt(`b sonni kiritng`);
+// let T =(a+b)/2;
+// console.log(T);
+
+
+
+// 9-masala
+// let R1 = + prompt(`R1 ni kiriting (R1>R2 bo'lsin)`);
+// let R2 = + prompt(`R2 ni kiriting (R2<R1 bo'lsin)`);
+
+// let S1 = R1*Math.PI;
+// let S2 = R2*Math.PI;
+// let S3 = Math.PI*(R1-R2);
+
+// console.log(S1);
+// console.log(S2);
+// console.log(S3);
+
+
+
+// 10-masala
+let t = + prompt(`gradusni kiriting`);
+
+let T = (t-32)*5/9;
+console.log(T) ;
